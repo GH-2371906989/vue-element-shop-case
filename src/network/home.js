d3afbe37f0c10b18c15030439ca152548f34afe4
GH-2371906989 <2371906989@qq.com>
2021-02-07 13:26:31 +0800

    '初始化项目'

---
+++ src/network/home.js
@@ -0,0 +1,8 @@
+import {request} from './request.js'
+
+export function getHomeMultiData() {
+	return request({
+		url: "/home/multidata",
+		method: "GET"
+	});
+}
---
+++ src/network/request.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+export function request(config) {
+  //1.axios创建实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    responseType: 'json'
+  })
+  //2.axios的拦截器
+  instance.interceptors.request.use(config=>{
+    //console. log(config);
+    // 1.比如config中的一些信息不符合服务器的要求
+    // 2.比如每次发送网络请求时，都希望在界面中显示一个请求的图标
+    //1 3.某些网络请求(比如登录(token)), 必须携带一 些特殊的信息
+    return config 
+  },err=>{
+    console.log(err)
+  })
+  instance.interceptors.response.use(res=>{
+    return res.data
+  },err=>{
+    console.log(err)
+  })
+  //3.发送真正的网络请求
+  return instance(config)
+}
+
+
+// export function request(config){
+//     const instance=axios.create({
+//       baseURL:'http://localhost:3000/',
+//       timeout:3000,
+//       responseType:'json'
+//     })
+//     return new Promise((resolve,reject)=>{
+//       instance(config)
+//         .then(data => {
+//           resolve(data.data)
+//         })
+//         .catch(err=>{
+//           reject(err)
+//         })
+//     })
+// }
